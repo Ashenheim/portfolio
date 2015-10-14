aa0d9367b63b96de09b647a6c497010aedbf740b
patt <patt.gerr@gmail.com>
2015-10-14 13:22:17 +0200

    update

--- gulp/config.js
+++ gulp/config.js
@@ -30,6 +30,10 @@ var $p = {
 
 
 module.exports = {
+
+    source: $p.source.index,
+    build: $p.build.index,
+
     /* ------------------------------
         Jekyll
     ------------------------------ */
@@ -93,30 +97,5 @@ module.exports = {
             $p.build.index + '*'
         ],
         settings: {}
-    },
-    /* ------------------------------
-        BrowserSync
-    ------------------------------ */
-    browsersync: {
-        server: {
-            baseDir: $p.build.index
-        },
-        host: "localhost",
-        online: true,
-        open: false,
-        notify: {
-            styles: [
-                'color: rgb(255, 255, 255)',
-                'position: fixed',
-                'z-index: 999999',
-                'bottom: 0px',
-                'left: 0px',
-                'font-size: 1em',
-                'background: rgba(0, 0, 0, 0.8)',
-                'font-family: arial, sans-serif',
-                'padding: 10px',
-                'box-shadow: 0 0 5px rgba(0,0,0,.3)'
-            ]
-        }
     }
 }
--- gulp/tasks/browsersync.js
+++ gulp/tasks/browsersync.js
@@ -3,13 +3,35 @@
 ==================================== */
 var gulp        = require('gulp');
 var browserSync = require('browser-sync');
-var config      = require('../config.js').browsersync;
+var config      = require('../config.js');
 
+var settings = {
+    server: {
+        baseDir: config.build
+    },
+    host: "localhost",
+    online: false,
+    open: false,
+    notify: {
+        styles: [
+            'color: rgb(255, 255, 255)',
+            'position: fixed',
+            'z-index: 999999',
+            'bottom: 0px',
+            'left: 0px',
+            'font-size: 1em',
+            'background: rgba(0, 0, 0, 0.8)',
+            'font-family: arial, sans-serif',
+            'padding: 10px',
+            'box-shadow: 0 0 5px rgba(0,0,0,.3)'
+        ]
+    }
+}
 
 /*
     Tasks & Functions
 ------------------------------------ */
 
 gulp.task('browsersync', function() {
-    browserSync(config);
+    browserSync(settings);
 });
